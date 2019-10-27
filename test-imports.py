4a42805ea337f93e534e11fc71c760f65720702e
Jamie Bliss <jamie@ivyleav.es>
2019-10-27 00:05:12 -0400

    Add some tests

---
+++ test-imports.py
@@ -0,0 +1,32 @@
+# Use a built-in module
+import socket
+import subprocess
+
+# Direct native modules
+import mmap
+import parser
+import readline
+import resource
+import termios
+
+# Use a native module
+import bz2
+import contextvars
+import crypt
+import csv
+import ctypes
+import curses
+import dbm
+import decimal
+import hashlib
+import json
+import lzma
+import multiprocessing
+import opcode
+import queue
+# import sqlite3
+# import ssl
+import tkinter
+import uuid
+
+import asyncio  # At the end because it's complex and uses lots of stuff
